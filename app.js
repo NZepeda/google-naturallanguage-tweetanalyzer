bfffc46789f08eb86074b0893a4ba2e02fcd1f31
Nestor Zepeda <NestorDZepeda@gmail.com>
2017-10-09 11:23:10 -0700

    Added newrelic packages

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('newrelic');
 var express = require("express");
 var mongoose = require("mongoose");
 var env = require("dotenv").config();
---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['user-tweet-sentiment-analyzer'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'f811673b0f8c7876324dec617d491b93e3e5d525',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
