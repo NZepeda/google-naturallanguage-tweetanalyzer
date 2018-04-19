b724a9ba12d3f86605b090c73a0b53505611a0b6
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-04-19 13:38:10 -0700

    User can now enter their twitter handle

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField' 
-
+import {Grid, Row, Col} from 'react-bootstrap'
 import logo from '../logo.svg';
 import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      text: ""
+    }
+
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(event){
+    console.log(event)
+    if(event.target.value !== "" && !event.target.value.includes('@')){
+      const newText = '@' + event.target.value
+      this.setState({text: newText})
+    }
+    else{
+      this.setState({text: event.target.value})
+    }
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Twitter Sentiment Analyzer</h1>
-        </header>
-        <p className="App-intro">
-          <TextField placeholder='@YourTwitterHandle ' />
-        </p>
-      </div>
+      <Grid className='App'>
+        <Row>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Twitter Sentiment Analyzer</h1>
+          </header>
+        </Row>
+        <Row>
+          <Col sm={12} md={8} mdOffset={2}>
+            <TextField id='twitter-handle-field' placeholder='@YourTwitterHandle' fullWidth={true} style={{fontSize: '2em', padding: '10px'}} value={this.state.text} onChange={this.onChange}></TextField>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
