b9ea1b130c030e5b0dad16a6275c9957ad3eb6a1
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-04-21 03:40:38 -0700

    Added labels to the pie chart

--- client/src/components/PieChart/SentimentPieChart.js
+++ client/src/components/PieChart/SentimentPieChart.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import {PieChart, Pie, Cell} from 'recharts';
+import {PieChart, Pie, Cell, Label} from 'recharts';
 
 class SentimentPieChart extends Component {
     constructor(props){
         super(props);
 
-        this.colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+        this.colors = ['#4CAF50', '#B71C1C', '#FFBB28', '#757575'];
         this.data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
         {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
     }
@@ -17,6 +17,7 @@ class SentimentPieChart extends Component {
                     dataKey="value"
                     data={this.data}
                     labelLine = {false}
+                    label={renderCustomizedLabel}
                     outerRadius={200}
                     fill= "#8884d8"
                 >
@@ -26,5 +27,19 @@ class SentimentPieChart extends Component {
         )
     }
 }
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+    console.log(index)
+    return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+    </text>
+    );
+};
 
 export default SentimentPieChart;
