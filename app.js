f1d32c80717bd03210abe6c97f921d60974bc664
Nestor Zepeda <NestorDZepeda@gmail.com>
2018-04-22 21:11:55 -0700

    Readying for production

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ googleController.authenticate();
 
 require('./server/routes')(app);
 
+if(process.env.NODE_ENV == 'production'){
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log("Listening on port: " + port);
 });
